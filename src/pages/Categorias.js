5d1ff2a0dd8d9656a8fb222bf187d36d0638eb19
SantiDure <santidure2002@gmail.com>
2023-07-06 20:07:55 -0300

    Warning en Categorias.js solucionado

--- src/pages/Categorias.js
+++ src/pages/Categorias.js
@@ -10,19 +10,17 @@ function Categorias() {
 
   const items = itemsFiltrados.map((item) => {
     return (
-      <>
-        <div key={item.id} className="item__layout">
-          <article className="item__container card">
-            <h2>{item.nombre}</h2>
-            <img src={item.img} alt={item.nombre}></img>
+      <div key={item.id} className="item__layout">
+        <article key={item.id} className="item__container card">
+          <h2>{item.nombre}</h2>
+          <img src={item.img} alt={item.nombre}></img>
 
-            <div>${item.precio}</div>
-            <Link to={`/productos/${item.id}`}>
-              <button className=" btn btn-primary">Ver detalles</button>
-            </Link>
-          </article>
-        </div>
-      </>
+          <div>${item.precio}</div>
+          <Link to={`/productos/${item.id}`}>
+            <button className=" btn btn-primary">Ver detalles</button>
+          </Link>
+        </article>
+      </div>
     );
   });
   return <div>{items}</div>;
