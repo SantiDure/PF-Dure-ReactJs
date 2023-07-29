6cd32734f8bc1edd9ac230df60afa4fb45e3dc57
SantiDure <santidure2002@gmail.com>
2023-07-28 21:30:47 -0300

    falta footer

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,42 +3,55 @@ import ItemCart from "../components/ItemCart/ItemCart";
 import { useCartContext } from "../context/CartContext";
 import { db } from "../config/firebase";
 import { addDoc, collection } from "firebase/firestore";
-import UserForm from "../components/UserForm/UserForm";
 import "./Cart.css";
 import { Link } from "react-router-dom";
 import img from "./assets/sad_119546.webp";
+import { useState } from "react";
 function Cart() {
   const { cart, totalPrice, clearCart } = useCartContext();
 
+  const [nombre, setNombre] = useState();
+  const [email, setEmail] = useState();
+  const [telefono, setTelefono] = useState();
+  const [cp, setCp] = useState();
+
   const ordenDeCompra = {
     datos: {
-      nombreUsuario: "santiago",
-      email: "aaa@aaa.com",
-      telefono: 1234,
+      nombreUsuario: nombre,
+      email: email,
+      telefono: telefono,
+      codigoPostal: cp,
     },
     orden: cart,
     total: totalPrice(),
   };
 
   const handleEmitirCompra = () => {
-    const ordenCollectionRef = collection(db, "ordenes");
-    addDoc(ordenCollectionRef, ordenDeCompra).then(({ id }) =>
+    if (!nombre || !email || !telefono || !cp) {
       Swal.fire({
-        title: "Tu orden fue realizada correctamente",
-        text: `Numero de pedido: ${id}`,
-        icon: "success",
-      })
-    );
-    clearCart();
+        title: `Orden fallida`,
+        text: `Debe completar sus datos antes de realizar el pedido`,
+        icon: "error",
+      });
+    } else {
+      const ordenCollectionRef = collection(db, "ordenes");
+      addDoc(ordenCollectionRef, ordenDeCompra).then(({ id }) =>
+        Swal.fire({
+          title: `${nombre} tu orden fue realizada correctamente`,
+          text: `Numero de pedido: ${id}`,
+          icon: "success",
+        })
+      );
+      clearCart();
+    }
   };
-
   if (cart.length === 0) {
     return (
       <>
         <div className="empty__cart">
           <h2>No hay productos actualmente en su carrito en su carrito</h2>
           <div>
-            <img src={img}></img>
+            <img src={img} alt="No hay productos en el carrito"></img>
           </div>
           <Link to={"/productos"}>
             {" "}
@@ -62,17 +75,87 @@ function Cart() {
 
   return (
     <>
-      <h3>Completa tus datos para realizar el pedido</h3>
+      <h3 className="cart__title">
+        Completa tus datos para realizar el pedido
+      </h3>
       <div className="cart__container">
         <div className="cart__layout">
-          <UserForm />
+          <form>
+            <div class="mb-3">
+              <label for="nombre" class="form-label">
+                Nombre
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="nombre"
+                aria-describedby="nombre"
+                onChange={(e) => {
+                  e.preventDefault();
+                  setNombre(e.target.value);
+                }}
+              />
+            </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="email"
+                aria-describedby="email"
+                onChange={(e) => {
+                  e.preventDefault();
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div class="mb-3">
+              <label for="telefono" class="form-label">
+                Teléfono
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="telefono"
+                onChange={(e) => {
+                  e.preventDefault();
+                  setTelefono(e.target.value);
+                }}
+              />
+            </div>
+            <div class="mb-3">
+              <label for="cp" class="form-label">
+                Codigo postal
+              </label>
+              <input
+                type="number"
+                class="form-control"
+                id="cp"
+                onChange={(e) => {
+                  e.preventDefault();
+                  setCp(e.target.value);
+                }}
+              />
+            </div>
+          </form>
 
           <div className="cart__card">{cards}</div>
         </div>
         <button className="btn btn-success" onClick={handleEmitirCompra}>
           Emitir compra
         </button>
-        <button className="btn btn-danger" onClick={() => clearCart()}>
+        <button
+          className="btn btn-danger"
+          onClick={() => {
+            clearCart();
+            Swal.fire({
+              title: "Se vacio tu carrito",
+              icon: "info",
+            });
+          }}
+        >
           Vaciar carrito
         </button>
       </div>
